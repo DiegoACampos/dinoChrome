cccf8390b51ba0d54cf9ee839ebca222a2142d13
DiegoACampos <diegoacampos.dc@gmail.com>
2022-03-18 01:19:55 -0300

    actualizado 18/3 1:20 am

--- proectGame1.js
+++ proectGame1.js
@@ -1,4 +1,7 @@
 const player = document.getElementById("player");
+const cactus1 = document.getElementById("cactus1");
+const fondo = document.getElementById("fondo");
+const buttonPlayStop = document.getElementById("buttonPlayStop");
 let score = 0;
 let scoreInterval;
 
@@ -6,11 +9,32 @@ document.addEventListener("click", function() {
     player.classList.add("playerJump")
 });
 
-document.addEventListener("animationend", () => {
+document.addEventListener('animationend', () => {
     player.classList.remove("playerJump")
 })
+
+function stopAnimation() {
+    cactus1.style.animationPlayState = 'paused';
+    player.style.animationPlayState = 'paused';
+    fondo.style.animationPlayState = 'paused';
+}
+
+function resumeGame() {
+    cactus1.style.animationPlayState = 'running';
+    player.style.animationPlayState = 'running';
+    fondo.style.animationPlayState = 'running';
+}
+
+buttonPlayStop.addEventListener('click', () => {
+    if (buttonPlayStop.classList.toggle("play")) {
+        resumeGame();
+    } else {
+        stopAnimation()
+    }
+
+});
 
 scoreInterval = setInterval(() => {
     score++;
     document.getElementById("score").innerText = score;
-}, 100);
+}, 1000);
